abab38515e4cba4d38eda51e2aaf5e915b2c5a04
iShinmazinger <mazinger05x72@gmail.com>
2026-04-11 23:35:35 -0500

    Commit Landing Page

---
+++ src/app/components/landing/bannerfinal/bannerfinal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bannerfinal',
+  imports: [],
+  templateUrl: './bannerfinal.html',
+  styleUrl: './bannerfinal.css',
+})
+export class Bannerfinal {
+
+}
---
+++ src/app/landing/landingpage/landingpage.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Intro } from '../../components/landing/intro/intro';
+import { Navbar } from '../../components/shared/navbar/navbar';
+import { Footer } from '../../components/shared/footer/footer';
+import { Howitworks } from '../../components/landing/howitworks/howitworks';
+import { Boxes } from '../../components/landing/boxes/boxes';
+import { Products } from '../../components/landing/products/products';
+import { Testimonials } from '../../components/landing/testimonials/testimonials';
+import { Bannerfinal } from '../../components/landing/bannerfinal/bannerfinal';
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [
+    Intro,
+    Navbar,
+    Footer,
+    Howitworks,
+    Boxes,
+    Products,
+    Testimonials,
+    Bannerfinal
+
+],
+  template: `
+    <app-navbar/>
+    <app-intro />
+    <app-howitworks />
+    <app-boxes />
+    <app-products /> 
+    <app-testimonials/>
+    <app-bannerfinal/> 
+    <app-footer/>
+    
+  `,
+  styleUrl: './landingpage.css',
+})
+export class Landingpage {
+
+}
