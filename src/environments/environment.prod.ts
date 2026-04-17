977b54335fce08fadf56b2a77020f322dc5d05b4
iShinmazinger <mazinger05x72@gmail.com>
2026-04-17 17:23:36 -0500

    Agregar environment para deploy en vercel

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  stripePublicKey: 'pk_test_51TLcCjLLP6Ak95kcX3seY7yayen6yOkNQ1uZHJLomV6QXrJgkbhaNxsWOQUqKaqou9ZBkuT7KxHZeVavUX75d9tL00UvWUW5AC',
+};
